60c9195081df02769f8d2b1520d3beb46c104fbb
Mahoo12138 <mahoo12138@qq.com>
2025-02-07 17:46:42 +0800

    fix avatar url forward

--- web/src/components/UserAvatar.tsx
+++ web/src/components/UserAvatar.tsx
@@ -7,11 +7,12 @@ interface Props {
 
 const UserAvatar = (props: Props) => {
   const { avatarUrl, className } = props;
+  const forwardAvatarUrl = avatarUrl && !avatarUrl.startsWith("http") ? `${import.meta.env.VITE_API_HOST}${avatarUrl}` : avatarUrl;
   return (
     <div className={cn(`w-8 h-8 overflow-clip rounded-xl`, className)}>
       <img
         className="w-full h-auto shadow min-w-full min-h-full object-cover dark:opacity-80"
-        src={avatarUrl || "/full-logo.webp"}
+        src={forwardAvatarUrl || "/full-logo.webp"}
         decoding="async"
         loading="lazy"
         alt=""
