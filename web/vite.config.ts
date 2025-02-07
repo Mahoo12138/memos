683e593fc03adedc5e71d16503ab0eeb0ab8e655
Mahoo12138 <mahoo12138@qq.com>
2025-02-07 18:00:13 +0800

    remove dev proxy server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,11 +3,6 @@ import { codeInspectorPlugin } from "code-inspector-plugin";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
-let devProxyServer = "http://localhost:8081";
-if (process.env.DEV_PROXY_SERVER && process.env.DEV_PROXY_SERVER.length > 0) {
-  console.log("Use devProxyServer from environment: ", process.env.DEV_PROXY_SERVER);
-  devProxyServer = process.env.DEV_PROXY_SERVER;
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,20 +18,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 3001,
-    // proxy: {
-    //   "^/api": {
-    //     target: devProxyServer,
-    //     xfwd: true,
-    //   },
-    //   "^/memos.api.v1": {
-    //     target: devProxyServer,
-    //     xfwd: true,
-    //   },
-    //   "^/file": {
-    //     target: devProxyServer,
-    //     xfwd: true,
-    //   },
-    // },
   },
   resolve: {
     alias: {
